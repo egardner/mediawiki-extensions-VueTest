7437a13842f3dce10f6271bc59c0eb8264f998cb
Tim Starling <tstarling@users.mediawiki.org>
2006-07-14 15:39:23 +0000

    Got rid of the MagicWord indexing constants (MAG_xxx), replaced them by string indexing, like messages. The associated change to the extensions, i.e. removing the MagicWordMagicWords hook and using string instead of integer indices, is in fact compatible with MW 1.6 and 1.7, which tells you something about the utility of the hook function.

--- Variable_hook.php
+++ Variable_hook.php
@@ -11,7 +11,6 @@ if (!defined('MEDIAWIKI')) die();
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
-$wgHooks['MagicWordMagicWords'][] = 'wfVariableHookWords';
 $wgHooks['MagicWordwgVariableIDs'][] = 'wfVariableHookVariables';
 $wgHooks['LanguageGetMagic'][] = 'wfVariableHookRaw';
 $wgHooks['ParserGetVariableValueSwitch'][] = 'wfVariableHookSwitch';
@@ -21,26 +20,20 @@ $wgExtensionCredits['variable'][] = array(
 	'author' => 'Ævar Arnfjörð Bjarmason'
 );
 
-function wfVariableHookWords( &$magicWords ) {
-	$magicWords[] = 'MAG_EXAMPLE';
-	
-	return true;
-}
-
 function wfVariableHookVariables( &$wgVariableIDs ) {
-	$wgVariableIDs[] = MAG_EXAMPLE;
+	$wgVariableIDs[] = 'example';
 
 	return true;
 }
 
 function wfVariableHookRaw( &$raw ) {
-	$raw[MAG_EXAMPLE] = array( 0, 'EXAMPLE' );;
+	$raw['example'] = array( 0, 'EXAMPLE' );;
 
 	return true;
 }
 
 function wfVariableHookSwitch( &$parser, &$varCache, &$index, &$ret ) {
-	if ( $index === MAG_EXAMPLE )
+	if ( $index === 'example' )
 		$ret = $varCache[$index] = wfVariableHookRet();
 	
 	return true;
