ed0aecda4c76cdfbc90ad56aa1e656e5bb6a8a3d
Meno25 <mfarag@users.mediawiki.org>
2008-09-07 08:42:25 +0000

    Add author info

--- FourFileTemplate/MyExtension.alias.php
+++ FourFileTemplate/MyExtension.alias.php
@@ -13,6 +13,20 @@ $aliases['en'] = array(
 	'MyExtension' => array( 'MyExtension' ),
 );
 
+/** Arabic (العربية)
+ * @author Meno25
+ */
+$aliases['ar'] = array(
+	'MyExtension' => array( 'امتدادي' ),
+);
+
+/** Egyptian Spoken Arabic (مصرى)
+ * @author Meno25
+ */
+$aliases['arz'] = array(
+	'MyExtension' => array( 'امتدادى' ),
+);
+
 /** Dutch (Nederlands) */
 $aliases['nl'] = array(
 	'MyExtension' => array( 'MijnExtensie' ),
