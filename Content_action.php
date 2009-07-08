afc917068e2e81d4ca75da4ff5b1b152fb8f5945
Ævar Arnfjörð Bjarmason <avar@users.mediawiki.org>
2009-07-08 15:36:47 +0000

    Fixed a 4 year old typo that jidanni pointed out

--- Content_action.php
+++ Content_action.php
@@ -13,7 +13,7 @@ $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
 	'name' => 'Content action hook',
 	'author' => 'Ævar Arnfjörð Bjarmason',
-	'desciprion' => 'Adds a new tab to each page',
+	'description' => 'Adds a new tab to each page',
 );
 
 
