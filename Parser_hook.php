4fc7f7b77772422024824d31777f88762a3ac85e
Brian Wolff <bawolff@users.mediawiki.org>
2012-02-03 19:29:59 +0000

    Fix the XSS vulnrability in the example extension. Also comment it more (Since its an example and all)

--- Parser_hook.php
+++ Parser_hook.php
@@ -20,22 +20,39 @@ $wgExtensionCredits['parserhook'][] = array(
 	'author' => 'Ævar Arnfjörð Bjarmason'
 );
 
+/* Register the registration function */
 $wgHooks['ParserFirstCallInit'][] = 'wfParserHook';
 
+/**
+ * The registration function.
+ */
 function wfParserHook( $parser ) {
 	$parser->setHook( 'hook' , 'wfParserHookParse' );
+	// Always return true.
 	return true;
 }
 
 /**
- * @param string $in The input passed to <hook>
- * @param array $argv The attributes of the <hook> element in array form
+ * @param String $data The input passed to <hook>
+ * @param Array $params The attributes of the <hook> element in array form
+ * @param Parser $parser Not used in this extension, but can be used to
+ * turn wikitext into html or do some other "advanced" stuff
+ * @param PPFrame $frame Not used in this extension, but can be used
+ * to see what template arguments ({{{1}}}) this hook was used with.
+ *
+ * @return String HTML to put in page at spot where <hook> tag is.
  */
-function wfParserHookParse( $data, $params, $parser ) {
+function wfParserHookParse( $data, $params, $parser, $frame ) {
+	// Very important to escape user data to prevent an XSS
+	// security vulnerability.
+	// print_r just turns an array into something readable.
+	$paramsEscaped = htmlspecialchars( print_r( $params, true ) );
+	$dataEscaped = htmlspecialchars( $data );
+
 	if ( !count( $params ) ) {
-		return $data;
+		return $dataEscaped;
 	} else {
-		return '<pre>' . $data . "\n" . print_r( $params, true ) . '</pre>';
+		return '<pre>' . $dataEscaped . "\n" . $paramsEscaped . '</pre>';
 	}
 }
 
