717e6fffa6c99d01991bc313cc02e1a117ceef4a
S Page <spage@wikimedia.org>
2015-05-26 01:21:06 -0700

    Add HTMLForm example code to special page
    I pasted in the code from
    https://www.mediawiki.org/wiki/HTMLForm#Example_usage,
    and converted to use 'ooui' layout.
    
    Bug: T100401
    Change-Id: I051c11f7327dff84c19e141a44c97897bcdd5736

--- specials/SpecialHelloWorld.php
+++ specials/SpecialHelloWorld.php
@@ -25,6 +25,70 @@ class SpecialHelloWorld extends SpecialPage {
 		$out->addHelpLink( 'How to become a MediaWiki hacker' );
 
 		$out->addWikiMsg( 'boilerplate-helloworld-intro' );
+
+		$formDescriptor = [
+			'myfield1' => [
+				'section' => 'section1/subsection',
+				'label-message' => 'testform-myfield1',
+				'type' => 'text',
+				'default' => 'Meep',
+			],
+			'myfield2' => [
+				'section' => 'section1',
+				'label-message' => 'testform-myfield2',
+				'class' => 'HTMLTextField', // same as type 'text'
+			],
+			'myfield3' => [
+				'class' => 'HTMLTextField',
+				'label' => 'Foo bar baz',
+			],
+			'myfield4' => [
+				'class' => 'HTMLCheckField',
+				'label' => 'This be a pirate checkbox',
+				'default' => true,
+			],
+			'omgaselectbox' => [
+				'class' => 'HTMLSelectField',
+				'label' => 'Select an oooption',
+				'options' => [
+					'pirate' => 'Pirates',
+					'ninja' => 'Ninjas',
+					'ninjars' => 'Back to the NINJAR!'
+				],
+			],
+			'omgmultiselect' => [
+				'class' => 'HTMLMultiSelectField',
+				'label' => 'Weapons to use',
+				'options' => [ 'Cannons' => 'cannon', 'Swords' => 'sword' ],
+				'default' => [ 'sword' ],
+			],
+			'radiolol' => [
+				'class' => 'HTMLRadioField',
+				'label' => 'Who do you like?',
+				'options' => [
+					'pirates' => 'Pirates',
+					'ninjas' => 'Ninjas',
+					'both' => 'Both'
+				],
+				'default' => 'pirates',
+			],
+		];
+
+		// $htmlForm = new HTMLForm( $formDescriptor, $this->getContext(), 'testform' );
+		$htmlForm = HTMLForm::factory( 'ooui', $formDescriptor, $this->getContext(), 'testform' );
+
+		$htmlForm->setSubmitText( 'Foo submit' );
+		$htmlForm->setSubmitCallback( [ 'SpecialHelloWorld', 'trySubmit' ] );
+
+		$htmlForm->show();
+	}
+
+	static function trySubmit( $formData ) {
+		if ( $formData['myfield1'] == 'Fleep' ) {
+			return true;
+		}
+
+		return 'HAHA FAIL';
 	}
 
 	protected function getGroupName() {
