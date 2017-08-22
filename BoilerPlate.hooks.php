48473e272fa51b4a1827123a51b302feb2988efa
Kunal Mehta <legoktm@member.fsf.org>
2017-08-21 18:09:11 -0700

    Cleanup incorrect usage of config_prefix, use ResourceFileModulePaths
    config_prefix is meant for a migration of variables using the legacy $eg
    prefix, not for each extension to define their own.
    
    Also use the ResourceFileModulePaths feature to specify all resources
    are in the modules/ directory so it doesn't need to be repeated for each
    file.
    
    Change-Id: Iab7b0ea68aae547ffe541e411910f2ff120de625

--- BoilerPlate.hooks.php
+++ BoilerPlate.hooks.php
@@ -9,10 +9,14 @@
 class BoilerPlateHooks {
 
 	/**
-	 * @return GlobalVarConfig
+	 * Hook: NameOfHook
+	 *
+	 * @param string $arg1 First argument
+	 * @param bool $arg2 Second argument
+	 * @param bool $arg3 Third argument
 	 */
-	public static function makeConfig() {
-		return new GlobalVarConfig( 'boilerplate' );
+	public static function onNameOfHook( $arg1, $arg2, $arg3 ) {
+		// Stub
 	}
 
 }
