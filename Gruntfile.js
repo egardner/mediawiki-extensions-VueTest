b666bb37f1216cf9cd502d60e9dcda334e907dbd
Gergő Tisza <gtisza@wikimedia.org>
2018-06-11 21:00:47 +0000

    Make stylelint not break in the absence of CSS files
    Change-Id: I635f4717b62c727d96112ca6ee7a82d244a1f5fe

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,8 @@ module.exports = function ( grunt ) {
 		},
 		stylelint: {
 			options: {
-				syntax: 'less'
+				syntax: 'less',
+				allowEmptyInput: true
 			},
 			all: [
 				'**/*.{css,less}',
